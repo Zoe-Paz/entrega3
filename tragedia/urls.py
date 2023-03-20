4ce915a243a0813ebe1c45daad18226457e5d27f
Zoe Paz <hola.zoepaz@gmail.com>
2023-03-20 19:38:23 -0300

    4.Corriccion de Imagenes

--- tragedia/urls.py
+++ tragedia/urls.py
@@ -1,26 +1,17 @@
-"""entrega_3 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+"""
 Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from  tragedia.views import *
+from tragedia import views
 
 urlpatterns = [
-    path(" ", index, name="index"),
-    path('inicio/', inicio, name="inicio"),
-    path('articulos/', articulos, name="articulos"),
-    path('autores/', autores, name="autores"),
-    path('categorias/', categorias, name="categorias"),
-    path('contacto/', contacto, name="contacto"),
+    path("", views.index, name="index"),
+    path('inicio/', views.inicio, name="inicio"),
+    path('articulos/', views.articulos, name="articulos"),
+    path('autores/', views.autores, name="autores"),
+    path('categorias/', views.categorias, name="categorias"),
+    path('contacto/', views.contacto, name="contacto"),
 ]
